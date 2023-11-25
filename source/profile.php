9e22a637760839e49ddd391183b2f42bba4b7e30
zaiolul <89912978+zaiolul@users.noreply.github.com>
2023-11-25 19:45:35 +0200

    styles.css path

--- source/profile.php
+++ source/profile.php
@@ -76,7 +76,7 @@ if ($_POST != null) {
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=9; text/html; charset=utf-8">
     <title>Pokalbis</title>
-    <link href="/styles.css" rel="stylesheet" type="text/css">
+    <link href="styles.css" rel="stylesheet" type="text/css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
