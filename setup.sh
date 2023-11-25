220b6f6063385968c7fe372650b791344e8fad39
zaiolul <89912978+zaiolul@users.noreply.github.com>
2023-11-25 20:12:53 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,8 +9,12 @@ fi
 
 cp ./source/* $SOURCE_DIR
 chmod o+x $SOURCE_DIR #if not set, need permissions to access
-
-mysql -u $1 -p$2 -e "CREATE DATABASE IF NOT EXISTS $DBNAME"
-mysql -u $1 -p$2 $DBNAME < db.sql
-
 systemctl restart apache2
+
+EXP="SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$DBNAME'"
+res=$(mysql -u stud -pstud -e "$EXP")
+
+if [ -n "$res" ]; then 
+    mysql -u $1 -p$2 -e "CREATE DATABASE IF NOT EXISTS $DBNAME"
+    mysql -u $1 -p$2 $DBNAME < db.sql
+fi
