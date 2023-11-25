cafa571a2924542c41dad82dae2adb5dee5b3074
zaiolul <89912978+zaiolul@users.noreply.github.com>
2023-11-25 19:31:56 +0200

    Update config.php

--- source/config.php
+++ source/config.php
@@ -1,7 +1,7 @@
 <?php
 define("DB_SERVER", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
+define("DB_USER", "stud");
+define("DB_PASS", "stud");
 define("DB_NAME", "it_proj");
 
 $user_roles=array(     
